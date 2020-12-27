ce2eb759f77c0fe0ee0b4d078a8bd2465e34d43b
yum <richardminyu@foxmail.com>
2020-12-27 23:38:01 +0800

    add py demo

--- ascii/ascii.py
+++ ascii/ascii.py
@@ -6,51 +6,52 @@ import argparse
 # 命令行输入参数处理
 parser = argparse.ArgumentParser()
 
-parser.add_argument('file') # 输入文件
-parser.add_argument('-o','--output') # 输出文件
-parser.add_argument('--width',type=int,default=80) # 默认输出字符宽
-parser.add_argument('--height',type=int,default=80) # 默认输出字符高
+parser.add_argument('file')  # 输入文件
+parser.add_argument('-o', '--output')  # 输出文件
+parser.add_argument('--width', type=int, default=80)  # 默认输出字符宽
+parser.add_argument('--height', type=int, default=80)  # 默认输出字符高
 
 # 获取参数
 args = parser.parse_args()
 
 IMG = args.file
-WIDTH=args.width
-HEIGHT=args.height
-OUTPUT=args.output
+WIDTH = args.width
+HEIGHT = args.height
+OUTPUT = args.output
 
-ascii_char=("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+ascii_char = (
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 
 # 将 256 灰度映射到 70 个字符上
-def get_char(r,g,b,alpha=256):
-  if alpha==0:
-    return ' '
-  length=len(ascii_char)
-  gray=int(0.2116 * r + 0.7152*g+0.0722*b)
-
-  unit=(256.0+1)/length
-  return ascii_char[int(gray/unit)]
-
-if __name__=='__main__':
-  im=Image.open(IMG)
-  im=im.resize((WIDTH,HEIGHT),Image.NEAREST)
-
-  txt=""
-
-  for i in range(HEIGHT):
-    for j in range(WIDTH):
-      txt+=get_char(*im.getpixel((j,i)))
-    txt+='\n'
-
-  print(txt)
-
-  # 将字符画输出到文件
-  if OUTPUT:
-    with open(OUTPUT,'w') as f:
-      f.write(txt)
-  else:
-    with open("output.txt","w") as f:
-      f.write(txt)
 
 
+def get_char(r, g, b, alpha=256):
+    if alpha == 0:
+        return ' '
+    length = len(ascii_char)
+    gray = int(0.2116 * r + 0.7152*g+0.0722*b)
 
+    unit = (256.0+1)/length
+    return ascii_char[int(gray/unit)]
+
+
+if __name__ == '__main__':
+    im = Image.open(IMG)
+    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
+
+    txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j, i)))
+        txt += '\n'
+
+    print(txt)
+
+    # 将字符画输出到文件
+    if OUTPUT:
+        with open(OUTPUT, 'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt", "w") as f:
+            f.write(txt)
---
+++ loveheart/love.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import string
+
+l = string.ascii_letters
+s = []
+s.append(l[34])
+s.append(l[11])
+s.append(l[14])
+s.append(l[21])
+s.append(l[4])
+s.append(l[24])
+s.append(l[14])
+s.append(l[20])
+s.insert(1, " ")
+s.insert(6, " ")
+
+string = "".join(s)
+# print(string)
+
+# print('\n'.join([''.join([('Love'[(x - y) % len('Love')] if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (
+#     x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+
+# print('\n'.join([''.join([('SB'[(x - y) % len('SB')] if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (
+#     x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
+
+txt = '\n'.join([''.join([('Love'[(x - y) % len('Love')] if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (
+    x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(30, -30, -1)])
+
+with open("output.txt", "w") as f:
+    f.write(txt)
