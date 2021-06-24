62a732c2369b9daa4bc7300bd6fc17ab0b678801
yum <richardminyu@foxmail.com>
2021-06-25 05:54:50 +0800

    add new demos

---
+++ algorithm/montecarlopi.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from random import random
+from time import perf_counter
+
+
+def calculate_pi(n=100):
+    print('\n n {}'.format(n))
+    hits = 0
+    start = perf_counter()
+    for i in range(1, n * n + 1):
+        x, y = random(), random()
+        dist = pow(x ** 2 + y ** 2, 0.5)
+        if dist <= 1.0:
+            hits += 1
+    pi = (hits * 4) / (n * n)
+    used_time = perf_counter() - start
+    return pi, used_time
+
+
+PI, used_time = calculate_pi(6000)
+print('\nuse Monte Carlo method to calculate PI: {}'.format(PI))
+print('use time: {} s'.format(used_time))
---
+++ ascii/importimg.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import turtle
+
+image = 'img/right.gif'
+screen = turtle.Screen()
+screen.bgpic(image)
+turtle.shape(image)
+turtle.mainloop()
