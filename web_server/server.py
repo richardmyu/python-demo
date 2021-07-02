9d9215230c3c2f31a362e94e90f7cebeee21d174
yum <richardminyu@foxmail.com>
2021-07-03 00:31:16 +0800

    update web server demo

--- web_server/server.py
+++ web_server/server.py
@@ -1,17 +1,47 @@
 # -*- coding:utf-8 -*_
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import sys, os
+import sys
+import os
 import subprocess
 
+
+# ---------------------------------------------------------------------
 
 class ServerException(Exception):
     """服务器内部错误"""
     pass
 
 
-class case_no_file(object):
-    """改路径不存在"""
+# ---------------------------------------------------------------------
+
+class base_case(object):
+    """条件处理基类"""
+
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = '"{0}" cannot be read: {1}'.format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    # 要求子类必须实现该接口
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+
+# ---------------------------------------------------------------------
+
+class case_no_file(base_case):
+    """路径不存在"""
 
     def test(self, handler):
         return not os.path.exists(handler.full_path)
@@ -20,17 +50,21 @@ class case_no_file(object):
         raise ServerException('"{0}" not found'.format(handler.path))
 
 
-class case_existing_file(object):
-    """该路径是文件"""
+# ---------------------------------------------------------------------
+
+class case_existing_file(base_case):
+    """路径是文件"""
 
     def test(self, handler):
         return os.path.isfile(handler.full_path)
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
 
-class case_always_fail(object):
+# ---------------------------------------------------------------------
+
+class case_always_fail(base_case):
     """所有情况都不符合时的默认处理类"""
 
     def test(self, handler):
@@ -40,30 +74,35 @@ class case_always_fail(object):
         raise ServerException('Unknown object "{0}"'.format(handler.path))
 
 
-class case_cgi_file(object):
+# ---------------------------------------------------------------------
+
+class case_cgi_file(base_case):
     """脚本文件处理"""
+
+    def run_cgi(self, handler):
+        data = subprocess.check_output(['python3', handler.full_path], shell=False)
+        handler.send_content(data)
 
     def test(self, handler):
         return os.path.isfile(handler.full_path) and \
                handler.full_path.endswith('.py')
 
     def act(self, handler):
-        handler.run_cgi(handler.full_path)
+        self.run_cgi(handler)
 
 
-class case_directory_index_file(object):
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
+# ---------------------------------------------------------------------
 
-    # 判断目标路径是否是目录 && 目录下是否有 index.html
+class case_directory_index_file(base_case):
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                os.path.isfile(self.index_path(handler))
 
-    # 响应 index.html 的内容
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(handler, self.index_path(handler))
 
+
+# ---------------------------------------------------------------------
 
 class RequestHandler(BaseHTTPRequestHandler):
     """请求路径合法则返回相应处理，否则返回错误页面"""
@@ -85,14 +124,9 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
+            # 得到完整的请求路径
             self.full_path = os.getcwd() + self.path
-
-            # if not os.path.exists(full_path):
-            #     raise ServerException('{0} not fund'.format(self.path))
-            # elif os.path.isfile(full_path):
-            #     self.handle_file(full_path)
-            # else:
-            #     raise ServerException('Unknown object "{0}"'.format(self.path))
+            # 遍历所有的情况并处理
             for case in self.Cases:
                 if case.test(self):
                     case.act(self)
@@ -100,40 +134,167 @@ class RequestHandler(BaseHTTPRequestHandler):
         except Exception as msg:
             self.handle_error(msg)
 
-    def create_page(self):
-        values = {
-            'date_time': self.date_time_string(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path
-        }
-        page = self.Page.format(**values)
-        return page
-
-    def send_content(self, page, status=200):
+    def send_content(self, content, status=200):
         self.send_response(status)
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', str(len(page)))
+        self.send_header('Content-Length', str(len(content)))
         self.end_headers()
-        self.wfile.write(page)
-
-    def handle_file(self, full_path):
-        try:
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = '"{0}" cannot be read: {1}'.format(self.path, msg)
-            self.handle_error(msg)
+        self.wfile.write(content)
 
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
         self.send_content(content.encode('utf-8'), 404)
 
-    def run_cgi(self, full_path):
-        data = subprocess.check_output(['python3', full_path], shell=False)
-        self.send_content(data)
+
+if __name__ == '__main__':
+    serverAddress = ('', 8080)
+    server = HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()  # -*- coding:utf-8 -*_
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+import os
+import subprocess
+
+
+# ---------------------------------------------------------------------
+
+class ServerException(Exception):
+    """服务器内部错误"""
+    pass
+
+
+# ---------------------------------------------------------------------
+
+class base_case(object):
+    """条件处理基类"""
+
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = '"{0}" cannot be read: {1}'.format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    # 要求子类必须实现该接口
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+
+# ---------------------------------------------------------------------
+
+class case_no_file(base_case):
+    """路径不存在"""
+
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        raise ServerException('"{0}" not found'.format(handler.path))
+
+
+# ---------------------------------------------------------------------
+
+class case_existing_file(base_case):
+    """路径是文件"""
+
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        self.handle_file(handler, handler.full_path)
+
+
+# ---------------------------------------------------------------------
+
+class case_always_fail(base_case):
+    """所有情况都不符合时的默认处理类"""
+
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        raise ServerException('Unknown object "{0}"'.format(handler.path))
+
+
+# ---------------------------------------------------------------------
+
+class case_cgi_file(base_case):
+    """脚本文件处理"""
+
+    def run_cgi(self, handler):
+        data = subprocess.check_output(['python3', handler.full_path], shell=False)
+        handler.send_content(data)
+
+    def test(self, handler):
+        return os.path.isfile(handler.full_path) and \
+               handler.full_path.endswith('.py')
+
+    def act(self, handler):
+        self.run_cgi(handler)
+
+
+# ---------------------------------------------------------------------
+
+class case_directory_index_file(base_case):
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+               os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        self.handle_file(handler, self.index_path(handler))
+
+
+# ---------------------------------------------------------------------
+
+class RequestHandler(BaseHTTPRequestHandler):
+    """请求路径合法则返回相应处理，否则返回错误页面"""
+    Cases = [case_no_file(),
+             case_cgi_file(),  # 注意顺序
+             case_existing_file(),
+             case_directory_index_file(),
+             case_always_fail()]
+
+    # 错误页面模板
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+    """
+
+    def do_GET(self):
+        try:
+            # 得到完整的请求路径
+            self.full_path = os.getcwd() + self.path
+            # 遍历所有的情况并处理
+            for case in self.Cases:
+                if case.test(self):
+                    case.act(self)
+                    break
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content.encode('utf-8'), 404)
 
 
 if __name__ == '__main__':
