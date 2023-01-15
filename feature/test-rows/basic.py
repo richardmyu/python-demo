fcf4d7bab8d666418dbab9814e7bfdab20e35cef
yum <richardminyu@foxmail.com>
2023-01-15 17:47:32 +0800

    refactor(/): update

---
+++ feature/test-rows/basic.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Rows 快速操作 csv 文件
+
+https://pythondict.com/python-data-analyze/rows/
+"""
+
+import rows
+
+# TODO: OverflowError: Python int too large to convert to C long
+
+cities = rows.import_from_csv("data/brazilian-cities.csv")
+rio_biggest_cities = [
+    city for city in cities if city.state == "RJ" and city.inhabitants > 500000
+]
+
+for city in rio_biggest_cities:
+    density = city.inhabitants / city.area
+    print(f"{city.city} ({density:5.2f} ppl/km²)")
