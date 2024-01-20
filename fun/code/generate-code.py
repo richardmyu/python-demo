ff17c36387845023c2aeea463b56d9f93cbd79af
yum <richardminyu@foxmail.com>
2024-01-20 22:02:31 +0800

    feat(/): add code script

---
+++ fun/code/generate-code.py
@@ -0,0 +1,170 @@
+# !/usr/bin/env python
+# coding= utf-8
+'''
+Author         : yum <richardminyu@foxmail.com>
+Date           : 2024-01-20 14:57:36
+LastEditors    : yum <richardminyu@foxmail.com>
+LastEditTime   : 2024-01-20 22:00:31
+Description    : 生成指定位数密码，写人 TXT
+'''
+
+# TODO: 有空继续完善
+import random
+import os
+
+CODE_PATH="./code.txt"
+CODED_PATH="./coded.txt"
+
+def generate_special_char(l):
+  """_生成 l 位特殊字符组_
+
+  Args:
+      l (_int_): _指定特殊字符个数_
+
+  Returns:
+      _str_: _特殊字符组合_
+  """
+  LIST_S_CHAR = ')!@#$%^&*(`~+-*'
+  length=len(LIST_S_CHAR)
+  r=''
+
+  for _ in range(l):
+    i=random.randint(0,length-1)
+    r+=str(LIST_S_CHAR[i:i+1])
+
+  return r
+
+
+def generate_number(l):
+  """_生成 l 位数字字符组_
+
+  Args:
+      l (_int_): _指定数字字符个数_
+
+  Returns:
+      _str_: _数字字符组合_
+  """
+  r=''
+
+  for _ in range(l):
+    i=random.randint(0,9)
+    r+=str(i)
+
+  return r
+
+
+def generate_char(l):
+  """_生成 l 位大小写字母字符组_
+
+  Args:
+      l (_int_): _指定大小写字母字符个数_
+
+  Returns:
+      _str_: _字母字符组合_
+  """
+  LOWERCASE_CHAR='abcdefghijklmnopqrstuvwxyz'
+  UPPERCASE_CHAR='ABCDEFJHIJKLMNOPQRSTUVWXYZ'
+  LIST_CHAR=LOWERCASE_CHAR+ UPPERCASE_CHAR +LOWERCASE_CHAR+ UPPERCASE_CHAR +LOWERCASE_CHAR
+  length=len(LIST_CHAR)
+  r=''
+
+  for _ in range(l):
+    i=random.randint(0,length-1)
+    r+=str(LIST_CHAR[i:i+1])
+
+  return r
+
+
+def mix_all_char(l=12):
+  """_数字、大小写字母和特殊字符混合组合_
+
+  Args:
+      l (int, optional): _description_. Defaults to 12.
+  """
+  MIN_S_CHAR_COUNT = 2
+  MIN_NUM_COUNT = 2
+  r=''
+
+  txt_s_char=generate_special_char(MIN_S_CHAR_COUNT)
+  txt_num=generate_number(MIN_NUM_COUNT)
+  txt_char=generate_char(l-MIN_S_CHAR_COUNT-MIN_NUM_COUNT)
+  txt=txt_s_char+txt_char+txt_num
+  list_index=[_ for _ in range(l)]
+  random.shuffle(list_index)
+
+  for _ in list_index:
+    r+=txt[_:_+1]
+
+  return r
+
+def mix_num_char(l=12):
+  """_数字、大小写字母混合组合_
+
+  Args:
+      l (int, optional): _description_. Defaults to 12.
+  """
+  MIN_NUM_COUNT = 4
+  r=''
+
+  txt_num=generate_number(MIN_NUM_COUNT)
+  txt_char=generate_char(l-MIN_NUM_COUNT)
+  txt=txt_char+txt_num
+  list_index=[_ for _ in range(l)]
+  random.shuffle(list_index)
+
+  for _ in list_index:
+    r+=txt[_:_+1]
+
+  return r
+
+
+def input_txt(filepath):
+  list_name=[]
+
+  if not os.path.exists(filepath):
+    print(f'当前环境：{os.getcwd()}')
+    print(f'文件路径：{filepath}')
+    print("文件不存在，请检查文件是否存在或文件路径是否正确。")
+  else:
+    try:
+      with open(filepath,'r',encoding="utf-8") as file:
+        list_name = file.readlines()
+    except FileNotFoundError:
+      print(f'当前环境：{os.getcwd()}')
+      print(f'文件路径：{filepath}')
+      print("文件未找到，请检查文件路径是否正确。")
+
+  return list_name
+
+def output_txt(filepath):
+  input_list=input_txt(CODE_PATH)
+  inputed_list=[]
+  input_diff=[]
+
+  if not os.path.exists(filepath):
+    with open(filepath,'w',encoding="utf-8") as file:
+      for line in input_list:
+        if line.strip("\n"):
+          all_char=''.join(mix_all_char())
+          num_char=''.join(mix_num_char())
+          txt=line.strip("\n")+" "+all_char+" "+num_char+"\n"
+          file.write(txt)
+  else:
+    with open(filepath,'r',encoding="utf-8") as file:
+      inputed_list=file.readlines()
+
+    for _ in input_list:
+      if _.strip("\n") not in ''.join(inputed_list):
+        input_diff.append(_)
+
+    with open(filepath,'a',encoding="utf-8") as file:
+      for line in input_diff:
+        if line.strip("\n"):
+          all_char=''.join(mix_all_char())
+          num_char=''.join(mix_num_char())
+          txt=line.strip("\n")+" "+all_char+" "+num_char+"\n"
+          file.write(txt)
+
+
+if __name__=="__main__":
+  output_txt(CODED_PATH)
---
+++ fun/code/one-code.py
@@ -0,0 +1,122 @@
+# !/usr/bin/env python
+# coding= utf-8
+'''
+Author         : yum <richardminyu@foxmail.com>
+Date           : 2024-01-20 21:57:04
+LastEditors    : yum <richardminyu@foxmail.com>
+LastEditTime   : 2024-01-20 22:00:20
+Description    : 在命令行中直接生成密码
+'''
+
+
+import random
+
+# TODO: 有空继续完善
+
+def generate_special_char(l):
+  """_生成 l 位特殊字符组_
+
+  Args:
+      l (_int_): _指定特殊字符个数_
+
+  Returns:
+      _str_: _特殊字符组合_
+  """
+  LIST_S_CHAR = ')!@#$%^&*(`~+-*'
+  length=len(LIST_S_CHAR)
+  r=''
+
+  for _ in range(l):
+    i=random.randint(0,length-1)
+    r+=str(LIST_S_CHAR[i:i+1])
+
+  return r
+
+
+def generate_number(l):
+  """_生成 l 位数字字符组_
+
+  Args:
+      l (_int_): _指定数字字符个数_
+
+  Returns:
+      _str_: _数字字符组合_
+  """
+  r=''
+
+  for _ in range(l):
+    i=random.randint(0,9)
+    r+=str(i)
+
+  return r
+
+
+def generate_char(l):
+  """_生成 l 位大小写字母字符组_
+
+  Args:
+      l (_int_): _指定大小写字母字符个数_
+
+  Returns:
+      _str_: _字母字符组合_
+  """
+  LOWERCASE_CHAR='abcdefghijklmnopqrstuvwxyz'
+  UPPERCASE_CHAR='ABCDEFJHIJKLMNOPQRSTUVWXYZ'
+  LIST_CHAR=LOWERCASE_CHAR+ UPPERCASE_CHAR +LOWERCASE_CHAR+ UPPERCASE_CHAR +LOWERCASE_CHAR
+  length=len(LIST_CHAR)
+  r=''
+
+  for _ in range(l):
+    i=random.randint(0,length-1)
+    r+=str(LIST_CHAR[i:i+1])
+
+  return r
+
+
+def mix_all_char(l=12):
+  """_数字、大小写字母和特殊字符混合组合_
+
+  Args:
+      l (int, optional): _description_. Defaults to 12.
+  """
+  MIN_S_CHAR_COUNT = 2
+  MIN_NUM_COUNT = 2
+  r=''
+
+  txt_s_char=generate_special_char(MIN_S_CHAR_COUNT)
+  txt_num=generate_number(MIN_NUM_COUNT)
+  txt_char=generate_char(l-MIN_S_CHAR_COUNT-MIN_NUM_COUNT)
+  txt=txt_s_char+txt_char+txt_num
+  list_index=[_ for _ in range(l)]
+  random.shuffle(list_index)
+
+  for _ in list_index:
+    r+=txt[_:_+1]
+
+  return r
+
+def mix_num_char(l=12):
+  """_数字、大小写字母混合组合_
+
+  Args:
+      l (int, optional): _description_. Defaults to 12.
+  """
+  MIN_NUM_COUNT = 4
+  r=''
+
+  txt_num=generate_number(MIN_NUM_COUNT)
+  txt_char=generate_char(l-MIN_NUM_COUNT)
+  txt=txt_char+txt_num
+  list_index=[_ for _ in range(l)]
+  random.shuffle(list_index)
+
+  for _ in list_index:
+    r+=txt[_:_+1]
+
+  return r
+
+
+if __name__=="__main__":
+  all_char=''.join(mix_all_char())
+  num_char=''.join(mix_num_char())
+  print(all_char, num_char)
